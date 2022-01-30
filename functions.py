001c6bd0125329a1a6955253bc7e675c3d111ba8
cheikh42 <cheikhmalaininec@gmail.com>
2022-01-30 19:35:27 +0100

    Initial commit

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from math import exp
 import numpy as np
 
 
@@ -14,5 +15,19 @@ def f2(x,y):
 def f3(x):
     return np.sin( x )
 
+## This function returns the values of the function adapted to a numpy array.
+## So if the input is X = [ x1 = [a, b, c],x2 = [a, b1, c1],x3 = [a2, b2, c2] ] 
+## This will give us f(X)=[f(x1), f(x2), f(x3)]
 def f4(x):
-    return np.exp( -np.linalg.norm(x-2) / (2/5) )
+    return np.exp(np.array([-np.linalg.norm(i-2)**2 for i in x]) / (2/5) )
+
+
+## This function plots the function in 3d and is adapted to np.meshgrid. 
+## It serves only for the purpose of visualization.
+def f4_3D(x,y):
+    return np.exp( - ( (x-2)**2 + (y-2)**2 )  / (2/5) ) 
+
+
+def f5(x,y):
+    return np.sin(x)+np.sin(y)
+
