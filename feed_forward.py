f721acc699e0689891d2ca3e3e600e3b7f2ca63e
cheikh42 <cheikhmalaininec@gmail.com>
2022-02-02 11:02:10 +0100

    final one

--- feed_forward.py
+++ feed_forward.py
@@ -1,8 +1,9 @@
-from py_compile import main 
+from py_compile import main
 from sklearn.model_selection import train_test_split
 import torch.nn.functional as F
 from torch import nn
-
+import brevitas.nn as qnn
+from brevitas.quant import Int8Bias as BiasQuant
 
 
 class NN0_0(nn.Module):
@@ -24,15 +25,15 @@ class NN1_F_D_1_ReLu(nn.Module):
     Using the ReLu activation function for all the layer
     """
 
-    def __init__(self, input_features=2):
+    def __init__(self, input_features=2, h=10):
         super().__init__()
-        self.layer1 = nn.Linear(input_features, 10)
-        self.layer2 = nn.Linear(10, 10)
-        self.layer3 = nn.Linear(10, 10)
-        self.layer4 = nn.Linear(10, 10)
-        self.layer5 = nn.Linear(10, 10)
+        self.layer1 = nn.Linear(input_features, h)
+        self.layer2 = nn.Linear(h, h)
+        self.layer3 = nn.Linear(h, h)
+        self.layer4 = nn.Linear(h, h)
+        self.layer5 = nn.Linear(h, h)
 
-        self.output = nn.Linear(10, 1)
+        self.output = nn.Linear(h, 1)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
@@ -47,17 +48,17 @@ class NN1_F_D_1_ReLu(nn.Module):
 class NN1_F_D_2_ReLu(nn.Module):
     """5 hidden layers, each has 10 neurons"""
 
-    def __init__(self, input_features=2):
+    def __init__(self, input_features=2, h=10):
         super().__init__()
-        self.layer1 = nn.Linear(input_features, 10)
+        self.layer1 = nn.Linear(input_features, h)
 
-        self.layer2 = nn.Linear(10, 10)
-        self.layer3 = nn.Linear(10, 10)
-        self.layer4 = nn.Linear(10, 10)
-        self.layer5 = nn.Linear(10, 10)
-        self.layer6 = nn.Linear(10, 10)
+        self.layer2 = nn.Linear(h, h)
+        self.layer3 = nn.Linear(h, h)
+        self.layer4 = nn.Linear(h, h)
+        self.layer5 = nn.Linear(h, h)
+        self.layer6 = nn.Linear(h, h)
 
-        self.output = nn.Linear(10, 1)
+        self.output = nn.Linear(h, 1)
 
     def forward(self, x):
 
@@ -78,19 +79,19 @@ class NN1_F_D_3_ReLu(nn.Module):
     8 hidden layers, each has 10 neurons
     """
 
-    def __init__(self, input_features=2):
+    def __init__(self, input_features=2, h=10):
         super().__init__()
-        self.layer1 = nn.Linear(input_features, 10)
+        self.layer1 = nn.Linear(input_features, h)
 
-        self.layer2 = nn.Linear(10, 10)
-        self.layer3 = nn.Linear(10, 10)
-        self.layer4 = nn.Linear(10, 10)
-        self.layer5 = nn.Linear(10, 10)
-        self.layer6 = nn.Linear(10, 10)
-        self.layer7 = nn.Linear(10, 10)
-        self.layer8 = nn.Linear(10, 10)
+        self.layer2 = nn.Linear(h, h)
+        self.layer3 = nn.Linear(h, h)
+        self.layer4 = nn.Linear(h, h)
+        self.layer5 = nn.Linear(h, h)
+        self.layer6 = nn.Linear(h, h)
+        self.layer7 = nn.Linear(h, h)
+        self.layer8 = nn.Linear(h, h)
 
-        self.output = nn.Linear(10, 1)
+        self.output = nn.Linear(h, 1)
 
     def forward(self, x):
 
@@ -113,22 +114,22 @@ class NN1_F_D_4_ReLu(nn.Module):
     10 hidden layers, each has 10 neurons
     """
 
-    def __init__(self, input_features=2):
+    def __init__(self, input_features=2, h=10):
         super().__init__()
-        self.layer1 = nn.Linear(input_features, 10)
+        self.layer1 = nn.Linear(input_features, h)
 
-        self.layer2 = nn.Linear(10, 10)
-        self.layer3 = nn.Linear(10, 10)
-        self.layer4 = nn.Linear(10, 10)
-        self.layer5 = nn.Linear(10, 10)
-        self.layer6 = nn.Linear(10, 10)
-        self.layer7 = nn.Linear(10, 10)
-        self.layer8 = nn.Linear(10, 10)
-        self.layer9 = nn.Linear(10, 10)
-        self.layer10 = nn.Linear(10, 10)
-        self.layer11 = nn.Linear(10, 10)
+        self.layer2 = nn.Linear(h, h)
+        self.layer3 = nn.Linear(h, h)
+        self.layer4 = nn.Linear(h, h)
+        self.layer5 = nn.Linear(h, h)
+        self.layer6 = nn.Linear(h, h)
+        self.layer7 = nn.Linear(h, h)
+        self.layer8 = nn.Linear(h, h)
+        self.layer9 = nn.Linear(h, h)
+        self.layer10 = nn.Linear(h, h)
+        self.layer11 = nn.Linear(h, h)
 
-        self.output = nn.Linear(10, 1)
+        self.output = nn.Linear(h, 1)
 
     def forward(self, x):
 
@@ -148,6 +149,7 @@ class NN1_F_D_4_ReLu(nn.Module):
 
         return x
 
+
 class NN0_F_S(nn.Module):
     """
     1 hidden layers each has 50 neurons
@@ -163,7 +165,7 @@ class NN0_F_S(nn.Module):
         x = self.output(x)
         return x
 
-    
+
 class NN1_F_S(nn.Module):
     """
     2 hidden layers each has 50 neurons by default
@@ -180,4 +182,3 @@ class NN1_F_S(nn.Module):
         x = F.relu(self.layer2(x))
         x = self.output(x)
         return x
- 
--- utils.py
+++
@@ -1,5 +0,0 @@
-""" This file contains utile basic functions for the challenge"""
-import numpy as np
-from tqdm import tqdm
-
-
